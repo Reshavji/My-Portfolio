ecc4d5d0a1f546f96ac2e16d0ef1043e46e879e9
Reshavji <reshavkumar9837@gmail.com>
2023-12-05 20:44:25 +0530

    Update

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -46,60 +46,72 @@ function About() {
         <div className="about-text">
           <h2 className="first-tag">Biography</h2>
           <p className="second-title">
-            I'm a Freelancer Front-end Developer with over 3 years of
-            experience. I'm from San Francisco. I code and create web elements
+            I'm a Freelancer Front-end Developer and a Cyber Security Learner. I code and create web elements
             for amazing people around the world. I like work with new people.
-            New people new Experiences. </p>
-            <p className="third-title">I'm a Freelancer Front-end Developer
-            with over 3 years of experience. I'm from San Francisco. I code and
-            create web elements for amazing people around the world. I like work
-            with new people. New people new Experiences.</p>
-       <div className="about-details">
-       <div class="column">
-      <table>
-        <tr>
-          <th>Name:-</th>
-          <td>Reshav Kumar</td>
-        </tr>
-        <tr>
-        <th>Phone:-</th>
-          <td><a href="tel:+91-9837758354">+91-9837758354</a></td>
-        </tr>
-        <tr>
-        <th>Email:-</th>
-          <td><a href="mailto:reshavkumar9837@gmail.com">reshavkumar9837@gmail.com</a></td>
-        </tr>
-        <tr>
-          <th>Address:-</th>
-          <td>Delhi</td>
-        </tr>
-      </table>
-    </div>
-    <div class="column">
-      <table>
-      <tr>
-          <th>Qualification-</th>
-          <td>Graduate</td>
-        </tr>
-        <tr>
-          <th>Website:-</th>
-          <td><a href="https://rjitech.blogspot.com">Rjitech</a></td>
-        </tr>
-        <tr>
-          <th>Freelance:-</th>
-          <td>Available</td>
-        </tr>
-        <tr>
-          <th></th>
-           </tr>
-        <tr>
-          <th></th>
-          <td></td>
-        </tr>
-        
-      </table>
-    </div>
-       </div>
+            New people new Experiences.{" "}
+          </p>
+          <p className="third-title">
+            I am eagerly seeking a dynamic and challenging opportunity to
+            contribute my skills and expertise to an esteemed organization. With
+            a relentless thirst for learning and a passion for growth, I aspire
+            to broaden my horizons and develop new techniques to enhance both my
+            own professional trajectory and the success of the organization.
+            Together, I believe we can achieve unprecedented heights of
+            excellence and create a lasting impact in our field.
+          </p>
+          <div className="about-details">
+            <div class="column">
+              <table>
+                <tr>
+                  <th>Name:-</th>
+                  <td>Reshav Kumar</td>
+                </tr>
+                <tr>
+                  <th>Phone:-</th>
+                  <td>
+                    <a href="tel:+91-9837758354">+91-9837758354</a>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Email:-</th>
+                  <td>
+                    <a href="mailto:reshavkumar9837@gmail.com">
+                      reshavkumar9837@gmail.com
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Address:-</th>
+                  <td>Delhi</td>
+                </tr>
+              </table>
+            </div>
+            <div class="column">
+              <table>
+                <tr>
+                  <th>Qualification-</th>
+                  <td>Graduate</td>
+                </tr>
+                <tr>
+                  <th>Website:-</th>
+                  <td>
+                    <a href="https://rjitech.blogspot.com">Rjitech</a>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Freelance:-</th>
+                  <td>Available</td>
+                </tr>
+                <tr>
+                  <th></th>
+                </tr>
+                <tr>
+                  <th></th>
+                  <td></td>
+                </tr>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
